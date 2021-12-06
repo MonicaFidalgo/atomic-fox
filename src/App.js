7c8c134a5855361b4d41500e90dcc99c676df506
Monica Fidalgo <monica.mofi@gmail.com>
2021-12-06 09:14:25 +0000

    readd navbar and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
+import NavBarComponent from "./components/NavBarComponent";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Blog from "./components/Blog/Blog";
 import Skills from "./components/Skills/Skills";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   const [load, upadateLoad] = useState(true);
   useEffect(() => {
@@ -17,18 +20,21 @@ function App() {
     }, 1200);
   }, []);
   return (
-    <div>
+    <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Home />
-        <About />
-        <Projects />
-        <Skills />
-        <Blog />
+        <NavBarComponent />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/project" component={Projects} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
         <Footer />
       </div>
-    </div>
-
+    </Router>
   );
 }
 
