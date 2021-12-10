8bd4e7d0870314574c051b2eda9d473ad89f6849
Monica Fidalgo <monicafidalgo@monicas-mbp.lan>
2021-12-10 14:35:21 +0000

    add icon

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,39 +5,33 @@ import { Container, Row, Col } from "react-bootstrap";
 import BlogCard from "./BlogsCards";
 
 function Blog() {
-  const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(true)
-  const DEV_TO_URL = "https://dev.to/api/articles?username=monicafidalgo"
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const DEV_TO_URL = "https://dev.to/api/articles?username=monicafidalgo";
 
   const getPosts = async () => {
     try {
-      const result = axios
-        .get(DEV_TO_URL)
-        .then(res => {
-          console.log(res)
-          return res.data
-        })
-      return result
+      const result = axios.get(DEV_TO_URL).then((res) => {
+        console.log(res);
+        return res.data;
+      });
+      return result;
     } catch (error) {
-      console.log("WE HAVE FETCH POST ERROR", error)
+      console.log("WE HAVE FETCH POST ERROR", error);
     }
-  }
+  };
 
   useEffect(() => {
-    getPosts().then(res => {
-      setPosts(res)
+    getPosts().then((res) => {
+      setPosts(res);
       setLoading(false);
     });
   }, []);
 
-
-  const blogCardsArray = posts.map(post => (
-    <Col md={3} className="project-card" key={post.id} >
-      <BlogCard
-        imgPath={post.cover_image}
-        link={post.url}
-        title={post.title}
-      /></Col>
+  const blogCardsArray = posts.map((post) => (
+    <Col md={3} className="project-card" key={post.id}>
+      <BlogCard imgPath={post.cover_image} link={post.url} title={post.title} />
+    </Col>
   ));
 
   if (!loading) {
@@ -47,7 +41,7 @@ function Blog() {
         <Container>
           <h1 className="project-heading">
             My Recent <strong className="purple">Blog </strong> Posts
-        </h1>
+          </h1>
           <p style={{ color: "white" }}>Do give a read to some of my blogs</p>
           <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
             {blogCardsArray}
@@ -56,9 +50,7 @@ function Blog() {
       </Container>
     );
   } else {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading ⌛...</div>;
   }
 }
 
